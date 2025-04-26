b4716e505748093a314c518d0ad276ebd37eb1fc
Deepaksharmadk <deepakdbg@gmail.com>
2025-04-26 14:47:22 +0530

    frontend completed

---
+++ src/components/Common/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <div className="bg-richblack-800">
+            <div className="mx-auto flex w-11/12 max-w-maxContent flex-row items-center justify-between p-7 text-sm text-richblack-400">
+                <div className="w-full text-center text-justify text-lg">
+                    <strong> Copyright © 2024 E-LMS | </strong> Made with ❤️
+                    Rahul, Niranjan, Aditya, Niranjan, Arvind, Desh Deepak,
+                    Nitish, Nishant, RN Raj & Manisha
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
